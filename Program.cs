33f8eefd7be5b274c4005a9e4eda066df92d710a
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-24 12:06:35 +0530

    Adding Welcome Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table");
+        }
+    }
+}
